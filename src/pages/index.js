4314fd9b5753a44417a861fbe7ed434a9695f003
Ratna Srivastava <srivastavaratna1611@gmail.com>
2025-03-18 14:23:01 +0530

    commited

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+<script src="//code.tidio.co/xtcsdpyrk34yzxxxnsz6ak7k7acpixyt.js" async></script>
+export { default as Home } from './Home';
+export { default as Products } from './Products';
+export { default as Product } from './Product';
+export { default as AboutPage } from './AboutPage';
+export { default as ContactPage } from './ContactPage';
+export { default as Cart } from './Cart';
+export { default as Login } from './Login';
+export { default as Register } from './Register';
+export { default as Checkout } from './Checkout';
+export { default as PageNotFound } from './PageNotFound';
